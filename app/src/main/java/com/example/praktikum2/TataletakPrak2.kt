f41880ed0a96ec8b4a957fb3bac9d44165091eeb
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-10-16 17:18:30 +0700

    Menambahkan start  end = 30.dp pada bagian TataletakColumn

--- app/src/main/java/com/example/praktikum2/TataletakPrak2.kt
+++ app/src/main/java/com/example/praktikum2/TataletakPrak2.kt
@@ -25,7 +25,7 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun TataletakColumn(modifier: Modifier) {
-    Column(modifier = modifier.padding( top = 20.dp, start = 30.dp,bottom = 20.dp{
+    Column(modifier = modifier.padding( top = 20.dp, start = 30.dp,bottom = 20.dp, end = 30.dp)){
         Text(text = "Komponen1")
         Text(text = "Komponen2")
         Text(text = "Komponen3")
