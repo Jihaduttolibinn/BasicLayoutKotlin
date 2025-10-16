eec9a694ea3320a0dc399a2c04361e06408f2663
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-10-16 16:12:28 +0700

    Menambahkan fun TataletakRow

--- app/src/main/java/com/example/praktikum2/TataletakPrak2.kt
+++ app/src/main/java/com/example/praktikum2/TataletakPrak2.kt
@@ -1,14 +1,35 @@
 package com.example.praktikum2
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TataletakColumn(modifier: Modifier) {
-    Column(modifier = modifier.padding((top = 20.dp)){
+    Column(modifier = modifier.padding( all = 20.dp)){
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
+
+@Composable
+fun TataletaRow(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
         Text(text = "Komponen1")
         Text(text = "Komponen2")
         Text(text = "Komponen3")
